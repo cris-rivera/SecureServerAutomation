2d565ebe215cf3b18ebaaaac618407687bbc3d58
Cris Rivera <cris.rivera131@gmail.com>
2021-03-30 17:16:50 -0500

    almost complete.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # SecureServerAutomation
+This is the practical project component of Cristian Rivera's Senior Capstone
+Research Project for the Cyber Operations program at the University of Arizona.
+This is an automation that will provision a Windows Server 2016 and enforce the
+DISA STIGs on the provisioned server immediately after creation.
--- server.tf
+++ server.tf
@@ -2,42 +2,61 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "crisResourceGroup" {
-  name      = "cris-resources"
-  location  = "westus"
-}
+#resource "azurerm_resource_group" "crisResourceGroup" {
+  #name      = "cris-resources"
+  #location  = "westus"
+#}
 
 resource "azurerm_virtual_network" "crisVNet" {
   name                = "cris-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.crisResourceGroup.location
-  resource_group_name = azurerm_resource_group.crisResourceGroup.name
+  location            = "westus"
+  resource_group_name = "imageRG"  
+
+  #location            = azurerm_resource_group.crisResourceGroup.location
+  #resource_group_name = azurerm_resource_group.crisResourceGroup.name
 }
 
 resource "azurerm_subnet" "crisSubNet" {
   name                 = "crisNet"
-  resource_group_name  = azurerm_resource_group.crisResourceGroup.name
+  #resource_group_name  = azurerm_resource_group.crisResourceGroup.name
+  resource_group_name  = "imageRG"
   virtual_network_name = azurerm_virtual_network.crisVNet.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
+
+resource "azurerm_public_ip" "CrisPublicIP" {
+  name                = "CrisPublicIp"
+  #resource_group_name = azurerm_resource_group.crisResourceGroup.name
+  #location            = azurerm_resource_group.crisResourceGroup.location
+  resource_group_name  = "imageRG"
+  location            = "westus"
+  allocation_method   = "Static"
+}
+
 resource "azurerm_network_interface" "cris_nic" {
   name                  = "cris_nic"
-  location              = azurerm_resource_group.crisResourceGroup.location
-  resource_group_name   = azurerm_resource_group.crisResourceGroup.name
+  #location              = azurerm_resource_group.crisResourceGroup.location
+  #resource_group_name   = azurerm_resource_group.crisResourceGroup.name
+  location            = "westus"
+  resource_group_name  = "imageRG"
 
   ip_configuration {
 
     name                          = "internal"
     subnet_id                     = azurerm_subnet.crisSubNet.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.CrisPublicIP.id
   }
 }
 
 resource "azurerm_windows_virtual_machine" "crisWindowsServer" {
   name                  = "crisVM"
-  resource_group_name   = azurerm_resource_group.crisResourceGroup.name
-  location              = azurerm_resource_group.crisResourceGroup.location
+  #resource_group_name   = azurerm_resource_group.crisResourceGroup.name
+  #location              = azurerm_resource_group.crisResourceGroup.location
+  resource_group_name  = "imageRG"
+  location            = "westus"
   size                  = "Standard_F2"
   admin_username        = "testadmin"
   admin_password        = "cyberOps@UofA"
@@ -50,11 +69,14 @@ resource "azurerm_windows_virtual_machine" "crisWindowsServer" {
     storage_account_type  = "Standard_LRS"
   }
 
-  source_image_reference {
-    publisher   = "MicrosoftWindowsServer"
-    offer       = "WindowsServer"
-    sku         = "2016-Datacenter"
-    version     = "latest"
-  }
+  source_image_id = "/subscriptions/316098cb-8835-448e-90e6-20a073644853/resourceGroups/imageRG/providers/Microsoft.Compute/images/Windows2016STIGImg"
+  
+
+  #source_image_reference {
+    #publisher   = "MicrosoftWindowsServer"
+    #offer       = "WindowsServer"
+    #sku         = "2016-Datacenter"
+    #version     = "latest"
+  #}
 
 }
