6077d4e635eeac484023b1dfe5c0dc6595c1fea9
Cris Rivera <cris.rivera131@gmail.com>
2021-04-08 15:17:26 -0500

    added to terraform commands to connect variables file to the rest of the configuration.

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "imageRG"
     storage_account_name = "tstate20029"
-    container_name       = "terraformstate"
+    container_name       = "tfst"
     key                  = "terraform.tfstate"
   }
 }
