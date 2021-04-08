1cf5d6a9950941702b0fba1bf66f5262fb211fa0
Cris Rivera <cris.rivera131@gmail.com>
2021-04-08 15:10:06 -0500

    fixed variable inconsistency.

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "imageRG"
     storage_account_name = "tstate20029"
-    container_name       = "tfstate"
+    container_name       = "terraformstate"
     key                  = "terraform.tfstate"
   }
 }
