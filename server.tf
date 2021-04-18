539249d7ffcdc3ad4b2110a5b4c40e8c9189242e
Cris Rivera <cris.rivera131@gmail.com>
2021-04-18 12:54:24 -0500

    Code cleanup

--- server.tf
+++ server.tf
@@ -2,24 +2,15 @@ provider "azurerm" {
   features {}
 }
 
-#resource "azurerm_resource_group" "crisResourceGroup" {
-  #name      = "cris-resources"
-  #location  = "westus"
-#}
-
 resource "azurerm_virtual_network" "crisVNet" {
   name                = "cris-network"
   address_space       = ["10.0.0.0/16"]
   location            = "westus"
   resource_group_name = "imageRG"  
-
-  #location            = azurerm_resource_group.crisResourceGroup.location
-  #resource_group_name = azurerm_resource_group.crisResourceGroup.name
 }
 
 resource "azurerm_subnet" "crisSubNet" {
   name                 = "crisNet"
-  #resource_group_name  = azurerm_resource_group.crisResourceGroup.name
   resource_group_name  = "imageRG"
   virtual_network_name = azurerm_virtual_network.crisVNet.name
   address_prefixes     = ["10.0.2.0/24"]
@@ -28,8 +19,6 @@ resource "azurerm_subnet" "crisSubNet" {
 
 resource "azurerm_public_ip" "CrisPublicIP" {
   name                = "CrisPublicIp"
-  #resource_group_name = azurerm_resource_group.crisResourceGroup.name
-  #location            = azurerm_resource_group.crisResourceGroup.location
   resource_group_name  = "imageRG"
   location            = "westus"
   allocation_method   = "Static"
@@ -37,8 +26,6 @@ resource "azurerm_public_ip" "CrisPublicIP" {
 
 resource "azurerm_network_interface" "cris_nic" {
   name                  = "cris_nic"
-  #location              = azurerm_resource_group.crisResourceGroup.location
-  #resource_group_name   = azurerm_resource_group.crisResourceGroup.name
   location            = "westus"
   resource_group_name  = "imageRG"
 
@@ -53,13 +40,9 @@ resource "azurerm_network_interface" "cris_nic" {
 
 resource "azurerm_windows_virtual_machine" "crisWindowsServer" {
   name                  = "crisVM"
-  #resource_group_name   = azurerm_resource_group.crisResourceGroup.name
-  #location              = azurerm_resource_group.crisResourceGroup.location
   resource_group_name  = "imageRG"
   location            = "westus"
   size                  = "Standard_F2"
-  #admin_username        = "testadmin"
-  #admin_password        = "cyberOps@UofA"
   
   admin_username = var.localUsr
   admin_password = var.localPwd
@@ -76,12 +59,4 @@ resource "azurerm_windows_virtual_machine" "crisWindowsServer" {
   source_image_id = "/subscriptions/316098cb-8835-448e-90e6-20a073644853/resourceGroups/imageRG/providers/Microsoft.Compute/images/finalSTIGImg"
   
   provision_vm_agent = true
-
-  #source_image_reference {
-    #publisher   = "MicrosoftWindowsServer"
-    #offer       = "WindowsServer"
-    #sku         = "2016-Datacenter"
-    #version     = "latest"
-  #}
-
 }
